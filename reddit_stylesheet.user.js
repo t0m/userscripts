5589a3c6b42e74a20c087bc1ce36beb86e3c1cb6
tom <tterrace@gmail.com>
2012-10-17 20:14:42 -0400

    clean up and remove IE-isms

--- reddit_stylesheet.user.js
+++ reddit_stylesheet.user.js
@@ -5,22 +5,18 @@
 // ==/UserScript==
 
 //
-var css = '#siteTable { max-width: 900px; margin-left:40px; }       \
-           .comments { display:none; }                              \
-           .rank { display:none; }                                  \
-           .midcol { display:none; }                                \
-           .entry { margin-bottom: 20px; }                          \
-           .spacer .sidebox { display:none; }                       \
-           .organic-listing { visibility:hidden; min-height:20px; } \
+var css = '#siteTable { max-width: 900px; margin-left:30px; margin-top: 20px; } \
+           .comments { display:none; }                                          \
+           .rank { display:none; }                                              \
+           .midcol { display:none; }                                            \
+           .entry { margin-bottom: 20px; }                                      \
+           .spacer .sidebox { display:none; }                                   \
+           .organic-listing { display:none; }                                   \
+           .link .flat-list { visibility:hidden; }                              \
            .spacer .sidecontentbox { display:none; }';
 
 var head = document.getElementsByTagName('head')[0];
 var style = document.createElement('style');
 style.type = 'text/css';
-
-if(style.styleSheet){
-    style.styleSheet.cssText = css;
-}else{
-    style.appendChild(document.createTextNode(css));
-}
+style.appendChild(document.createTextNode(css));
 head.appendChild(style);
